06ec656ce8fb225180c1e3b860b065e090a11cbc
guistetter <guilherme_stetter@hotmail.com>
2020-09-23 16:52:30 -0300

    refatorando metodo preview img

--- public/scripts.js
+++ public/scripts.js
@@ -14,30 +14,45 @@ const Mask = {
 }
 
 const PhotosUpload = {
+  preview: document.querySelector('#photos-preview'),
   //validando limit fotos
   uploadLimit: 6,
   handleFileInput(event){
     const {files: fileList } = event.target
-    const {uploadLimit} = PhotosUpload
-    if(fileList.length > uploadLimit){
-      alert(`Envie no máximo ${uploadLimit} fotos`)
-      event.preventDefault()
-      return
-    }
+
+    if(PhotosUpload.hasLimit(event)) return
     //fazendo o preview de photos
     Array.from(fileList).forEach(file => {
       const reader = new FileReader()
+
       reader.onload = () => {
         const image = new Image()
         image.src = String(reader.result)
-        const div = document.createElement('div')
-        div.classList.add('photo')
-        div.onclick = () =>  alert('cliquei')
-        div.appendChild(image)
-        document.querySelector('#photos-preview').appendChild(div)
+        
+        const div = PhotosUpload.getContainer(image) 
+        PhotosUpload.preview.appendChild(div)
+        
       }
+
       reader.readAsDataURL(file)
     })
+  },
+  hasLimit(event){
+    const {uploadLimit} = PhotosUpload
+
+    if(fileList.length > uploadLimit){
+      alert(`Envie no máximo ${uploadLimit} fotos`)
+      event.preventDefault()
+      return true
+    }
+    return false
+  },
+  getContainer(image){
+    const div = document.createElement('div')
+    div.classList.add('photo')
+    div.onclick = () =>  alert('cliquei')
+    div.appendChild(image)
+    return div
   }
 }
 
