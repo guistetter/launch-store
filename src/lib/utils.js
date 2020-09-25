81ff79c71f32d5a593a01eb8795d4a88b4b85ba4
guistetter <guilherme_stetter@hotmail.com>
2020-09-25 10:15:24 -0300

    lib para formatar data e hora

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,23 +1,24 @@
 module.exports = {
-  age(timestamp){
-    const today = new Date()
-    const birthDate = new Date(timestamp)
-  
-    let age = today.getFullYear() - birthDate.getFullYear()
-    const month = today.getMonth()- birthDate.getMonth()
-  
-    if(month < 0 || month == 0 && today.getDate() <=  birthDate.getDate()){
-      age = age - 1
-    }
-    return age
-  },
   date(timestamp){
-    const date = Date(timestamp)
+    const date = new Date(timestamp)
     
     const year = date.getUTCFullYear()
-    const month = date.getUTCMonth() + 1
-    const day = date.getUTCDate()
+    const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+    const day = `0${date.getUTCDate()}`.slice(-2)
+    const hour = date.getHours()
+    const minutes = date.getMinutes()
+
     console.log(`${year}-${month}-${day}`)
+    return{
+      day,
+      month,
+      year,
+      hour,
+      minutes,
+      iso:`${year}-${month}-${day}`,
+      birthDay: `${day}/${month}`,
+      format: `${day}/${month}/${year}`
+    }
   },
   formatPrice(price){
     return new Intl.NumberFormat('pt-BR', {
