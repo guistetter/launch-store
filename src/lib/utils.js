fdd44cb5e4ffd773f5096176a801ac0e359ebb83
guistetter <guilherme_stetter@hotmail.com>
2020-09-26 16:18:10 -0300

    removendo timezone utc

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,10 +2,10 @@ module.exports = {
   date(timestamp){
     const date = new Date(timestamp)
     
-    const year = date.getUTCFullYear()
-    const month = `0${date.getUTCMonth() + 1}`.slice(-2)
-    const day = `0${date.getUTCDate()}`.slice(-2)
-    const hour = date.getHours()
+    const year = date.getFullYear()
+    const month = `0${date.getMonth() + 1}`.slice(-2)
+    const day = `0${date.getDate()}`.slice(-2)
+    const hour = date.getHours() - 3
     const minutes = date.getMinutes()
 
     console.log(`${year}-${month}-${day}`)
