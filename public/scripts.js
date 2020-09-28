276dbcbbbcd5dcf645f48b77be431505f6a0f683
guistetter <guilherme_stetter@hotmail.com>
2020-09-28 13:00:44 -0300

    Finalizando lightbox

--- public/scripts.js
+++ public/scripts.js
@@ -120,9 +120,27 @@ const ImageGallery = {
     target.classList.add('active')
 
     ImageGallery.highlight.src = target.src
+    Lightbox.img.src = target.src
   }
 }
 
+const Lightbox = {
+  target: document.querySelector('.lightbox-target'),
+  img: document.querySelector('.lightbox-target img'),
+  closeButton: document.querySelector('.lightbox-target a.lightbox-close'),
+  open(){
+    Lightbox.target.style.opacity = 1
+    Lightbox.target.style.top = 0
+    Lightbox.target.style.bottom = 0
+    Lightbox.closeButton.style.top = 0
+  },
+  close(){
+    Lightbox.target.style.opacity = 0
+    Lightbox.target.style.top = "-100%"
+    Lightbox.target.style.bottom = "initial"
+    Lightbox.closeButton.style.top = "-80px"
+  }
+}
 /*
 const input = document.querySelector('input[name="price"]')
 input.addEventListener("keydown", function(e){
