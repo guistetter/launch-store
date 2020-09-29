b19817d48842446762ea0b3127b1f02ff1a75966
guistetter <guilherme_stetter@hotmail.com>
2020-09-29 11:04:56 -0300

    movendo rotas de users

--- src/routes/index.js
+++ src/routes/index.js
@@ -25,23 +25,5 @@ routes.get("/ads/create", function(req, res){
   return res.redirect("/products/create")
 })
 
-//login/logout
-routes.get('/login', SessionController.loginForm)
-routes.post('/login', SessionController.login)
-routes.post('/logout', SessionController.logout)
-
-//reset password/forgot
-routes.get('/forgot-password', SessionController.forgotForm)
-routes.post('/password-reset', SessionController.resetForm)
-routes.post('/forgot-password', SessionController.forgot)
-routes.post('/password-reset', SessionController.reset)
-
-//user register UserController
-routes.get('/register', UserController.registerForm)
-routes.post('/register', UserController.post)
-
-routes.get('/', UserController.show)
-routes.put('/', UserController.update)
-routes.delete('/', UserController.delete)
 
 module.exports = routes
