c56b3bd19f03fb48191b2a8290e8b9e606ac54d2
guistetter <guilherme_stetter@hotmail.com>
2020-10-05 18:43:17 -0300

    fix

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,18 +1,27 @@
 const db = require("../../config/db");
+
 module.exports = {
   async findOne(filters) {
-    let query = "select * from users";
+    try {
+      let query = `SELECT *
+      FROM users`;
 
-    Object.keys(filters).map((key) => {
-      //Where | or
-      query = `${query}
-      ${key}`;
-
-      Object.keys(filters[key]).map((field) => {
-        query = `${query} ${field} = '${filters[key][field]}'`;
+      Object.keys(filters).map((key) => {
+        // WHERE | OR | AND
+        query = `${query}
+        ${key}
+      `;
+        Object.keys(filters[key]).map((field) => {
+          // email | cpf_cnpj
+          query = `${query} users.${field} = '${filters[key][field]}'`;
+        });
       });
-    });
-    const results = await db.query(query);
-    return results.rows[0];
+
+      const results = await db.query(query);
+
+      return results.rows[0];
+    } catch (err) {
+      console.error(err);
+    }
   },
 };
