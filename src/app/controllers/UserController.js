e9b911f431d1b371ae6b64f4ffa3126816205458
guistetter <guilherme_stetter@hotmail.com>
2020-10-05 17:33:23 -0300

    create controller e metodo post

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,27 @@
+const User = require("../models/User");
 module.exports = {
-  registerForm(req,res){
-    return res.render("user/register")
-  }
-}
+  registerForm(req, res) {
+    return res.render("user/register");
+  },
+
+  async post(req, res) {
+    //check if has all fields
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send("Preencha todos os campos");
+      }
+    }
+
+    //check if user exists[emai, cpf_cnpj]
+    const { email, cpf_cnpj } = req.body;
+
+    const user = await user.findOne({
+      where: email,
+      or: { cpf_cnpj },
+    });
+
+    //check if password match
+  },
+};
