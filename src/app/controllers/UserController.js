7e406811f5acde1257c4000a09ce5e78baa7816c
guistetter <guilherme_stetter@hotmail.com>
2020-10-05 18:58:55 -0300

    refactor passando validacao p/ middleware user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,32 +1,9 @@
-const User = require("../models/User");
 module.exports = {
   registerForm(req, res) {
     return res.render("user/register");
   },
 
   async post(req, res) {
-    //check if has all fields
-    const keys = Object.keys(req.body);
-
-    for (key of keys) {
-      if (req.body[key] == "") {
-        return res.send("Preencha todos os campos");
-      }
-    }
-
-    //check if user exists[emai, cpf_cnpj]
-    let { email, cpf_cnpj, password, passwordRepeat } = req.body;
-    cpf_cnpj = cpf_cnpj.replace(/\D/g, "");
-
-    const user = await User.findOne({
-      where: { email },
-      or: { cpf_cnpj },
-    });
-
-    if (user) return res.send("User exists");
-
-    //check if password match
-    if (password != passwordRepeat) return res.send("Password Mismatch");
     return res.send("passou");
   },
 };
