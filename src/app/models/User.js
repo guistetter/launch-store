db6db13feeb8cb3b0daf6f50738af2715e1f52db
guistetter <guilherme_stetter@hotmail.com>
2020-10-07 15:41:26 -0300

    metodo create user

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 const db = require("../../config/db");
+const { hash } = require("bcryptjs");
 
 module.exports = {
   async findOne(filters) {
@@ -22,6 +23,38 @@ module.exports = {
       return results.rows[0];
     } catch (err) {
       console.error(err);
+    }
+  },
+  async create(data) {
+    try {
+      const query = `
+      insert into users (
+        name,
+        email,
+        password,
+        cpf_cnpj,
+        cep,
+        address
+      ) values ($1, $2, $3, $4, $5, $6)
+      returning id
+    `;
+
+      //hash de senha
+      const passwordHash = await hash(data.password, 8);
+
+      const values = [
+        data.name,
+        data.email,
+        passwordHash,
+        data.cpf_cnpj.replace(/\D/g, ""),
+        data.cep.replace(/\D/g, ""),
+        data.address,
+      ];
+
+      const results = await db.query(query, values);
+      return results.rows[0].id;
+    } catch (err) {
+      return console.log(err);
     }
   },
 };
