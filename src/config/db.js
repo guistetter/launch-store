b631de147448f1e0c712c2b2f7af5bf276ca4743
guistetter <guilherme_stetter@hotmail.com>
2020-10-08 10:04:05 -0300

    refactor lint

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
-require("dotenv/config")
-const { Pool } = require('pg')
+require("dotenv/config");
+const { Pool } = require("pg");
 module.exports = new Pool({
   user: "postgres",
   password: process.env.POSTGRESS_PASS,
   host: "localhost",
   port: "5432",
-  database: "launchstoreDb"
-})
+  database: "launchstoreDb",
+});
